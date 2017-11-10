505666acddb65ab066d635a5278825f0b0abc565
unknown <apresh.roakalla@appshark.com>
2017-11-10 17:01:08 +0530

    'modified'

--- post_deploy_test/test.js
+++ post_deploy_test/test.js
@@ -1,15 +1,17 @@
-var supertest = require('supertest');
-var should = require('should');
+var supertest = require("supertest");
+var should = require("should");
 
 var build_url = process.env.BUILD_URL;
 //var build_url = "http://localhost:3000/";
 var request = supertest.agent(build_url);
 
+
+console.log(build_url);
 describe("Test 1",function(){
 
   it("Check Company Api Status Code 200", function (done) {
       request
-      .get('test')
+      .get('test')      
       .expect(200)
       .end(function(err, res) {
         if (err) return done(err);
@@ -20,7 +22,7 @@ describe("Test 1",function(){
   
   it("Check User Api Status Code 200", function (done) {
       request
-      .get('test')      
+      .get('test')  
       .expect(200)
       .end(function(err, res) {
         if (err) return done(err);
