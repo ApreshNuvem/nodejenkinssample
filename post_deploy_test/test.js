a00b958635a0048532879812f6045c16450ae2e2
unknown <apresh.roakalla@appshark.com>
2017-11-10 16:31:20 +0530

    'modified'

--- post_deploy_test/test.js
+++ post_deploy_test/test.js
@@ -5,7 +5,6 @@ var build_url = process.env.BUILD_URL;
 //var build_url = "http://localhost:3000/";
 var request = supertest.agent(build_url);
 
-
 describe("Test 1",function(){
 
   it("Check Company Api Status Code 200", function (done) {
