38679bbbbb14d18b8bc5c01137948886a44d4783
unknown <apresh.roakalla@appshark.com>
2017-11-14 18:39:46 +0530

    'clean'

--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -171,8 +171,8 @@ res.send("<h2>Welcome to newapi1 </h2>");
 /**
  * Default Homepage
  */
-app.get('/newap2', function(req, res, next) {
-res.send("<h2>Welcome to newapi2 </h2>");
+app.get('/newap23', function(req, res, next) {
+res.send("<h2>Welcome to newapi23 </h2>");
 
 });
 
